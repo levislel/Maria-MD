bec0932c86fb344dac14f2f225d069757a8bf58e
levislel <164642225+levislel@users.noreply.github.com>
2024-05-07 20:29:44 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "22545752448"],
+global.ownername = process.env.OWNER_NAME || "𝙂𝞢𝞜𝙏𝙇𝞢𝞛𝞓𝞜. 🕷️",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -12,11 +12,11 @@ global.location = "Indian"
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "𝙂𝞢𝞜𝙏𝙇𝞢𝞛𝞓𝞜. 🕷️",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "𝙂𝞢𝞜𝙏𝙇𝞢𝞛𝞓𝞜. 🕷️",
 //console view/theme
-global.themeemoji = '🧩'
+global.themeemoji = '🧘'
 global.wm = "Ayush botz inc."
 
 //theme link
@@ -31,9 +31,9 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
@@ -51,8 +51,8 @@ global.mess = {
 }
 
 module.exports = {
-ownernumber: global.ownernumber,
-ownername: global.ownername,
+ownernumber: global.22545752448,
+ownername: global.𝙂𝞢𝞜𝙏𝙇𝞢𝞛𝞓𝞜. 🕷️,
 sessionId: process.env.id,
 AUTO_BLOCK : process.env.PM_BLOCKER,
 STATUS_SAVER : "true",
